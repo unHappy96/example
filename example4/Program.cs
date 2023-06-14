7fb8d56c55a7755775b467610daaac525315f973
Happy <ya.elikov@yandex.ru>
2023-06-14 15:58:31 +0300

    Написали программу, которая показывает все четные числа

---
+++ example4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число, ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+var list = new List<int>();
+
+while (number > 0) {
+    if ((number%2)==0){
+        list.Add(number);
+    }
+
+    number = number - 1;
+}
+
+list.ForEach(Console.WriteLine);
