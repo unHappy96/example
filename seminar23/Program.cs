766415d24d22c65c4c14e7a2c9733ef3357d5ee6
Happy <ya.elikov@yandex.ru>
2023-06-15 16:07:48 +0300

    Написали программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным

---
+++ seminar23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите день недели");
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value%6==0 || value%7==0) {
+    Console.WriteLine("Это выходной день");
+} else {
+    Console.WriteLine("Это будний день");
+}
