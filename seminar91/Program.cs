a4a8214315bbdc62f208314e20b16654a7c974b1
Happy <ya.elikov@yandex.ru>
2023-07-12 17:40:03 +0300

    Семинар 9, первая задача

---
+++ seminar91/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int ReadInt(string text){
+
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int arg1, int arg2){
+    if (arg2 == arg1) return arg1;
+    else if (arg2 == 0) return 0;
+    else return arg2 + Sum(arg1, arg2-1);
+}
+
+//-------------
+
+int M = ReadInt("Введите начальное значение M: ");
+int N = ReadInt("Введите конечное значение N: ");
+
+Console.WriteLine(Sum(M, N));
